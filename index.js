cff53121dd37d1a0a7c6536f6be55a64d5267e4a
ian-ramos <ramos.ianc@gmail.com>
2017-10-09 14:18:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function logDriverNames(array) {
+  return array.forEach(function(element) {
+    console.log(element['name'])
+  })
+};
+
+function logDriversByHometown(array, hometown) {
+  const filtered = array.filter(function (element) {
+    return element['hometown'] === hometown
+  });
+  filtered.forEach(function(element) {
+    console.log(element['name'])
+  })
+}
+
+function driversByRevenue(array) {
+  return array.slice(0).sort(function(a, b) {
+    return a.revenue - b.revenue
+  })
+}
+
+function driversByName(array) {
+  return array.slice(0).sort(function(a, b) {
+    return a.name.localeCompare(b.name);
+  })
+}
+
+function totalRevenue(array) {
+  return array.reduce(function(acc, el) {
+    return acc + el.revenue
+  }, 0);
+};
+
+function averageRevenue(array) {
+  return totalRevenue(array) / array.length
+};
